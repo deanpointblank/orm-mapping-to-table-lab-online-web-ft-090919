3e0692ae7e549903c62c6993fe90ae86441ba164
Dean L Betty <deanlloyd64@gmail.com>
2019-10-11 22:45:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-    @id
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
   def self.create(name, grade)
